7647d279f2aa09ed16be8bf8a9e1871bf99bfd13
Parallels <sergej.keller@tuhh.de>
2022-06-21 08:37:08 +0200

    added older calibration images to image set and updated calibration xml

--- jetson_nano/catkin_ws/src/camera/scripts/calib_samples/raw/imagecut.py
+++ jetson_nano/catkin_ws/src/camera/scripts/calib_samples/raw/imagecut.py
@@ -1,7 +1,7 @@
 import cv2   
 # Read the image
 
-for i in range(1,7):
+for i in range(1,10):
 	img = cv2.imread('video_shot_' + str(i) + '.png')
 	print(img.shape)
 	height = img.shape[0]
@@ -12,5 +12,5 @@ for i in range(1,7):
 	img_right = img[:, :width_cutoff] # right
 	img_left = img[:, width_cutoff:] # left
 
-	cv2.imwrite("right_cut/video_right_" + str(i) + ".png", img_right)
-	cv2.imwrite("left_cut/video_left_" + str(i) + ".png", img_left)
+	cv2.imwrite("../right_cut/video_right_" + str(i) + ".png", img_right)
+	cv2.imwrite("../left_cut/video_left_" + str(i) + ".png", img_left)
